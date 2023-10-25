169ccbbec796f1b8ee205cdd4c8fd4da10e7b28d
chang10th <chang10thomas@gmail.com>
2023-10-25 12:24:23 +0200

    Correction bug color

--- app.js
+++ app.js
@@ -2,11 +2,9 @@ import chalk from "chalk";
 
 const camarades = ['Elie', 'Hava', 'Jérôme', 'Jayson'];
 
-const chalkColors = ['red','cyanBright','greenBright','magenta','yellow','cyan',''];
+const chalkColors = ['red', 'cyanBright', 'greenBright', 'magenta', 'yellow', 'cyan', 'blue'];
 
 camarades.forEach((camarade, index) => {
-  const color = index % 6 + 1;
-  console.log(chalk.hex(chalkColors[color])(camarade));
+  const color = index % chalkColors.length; // Utilisation du modulo pour boucler à travers les couleurs
+  console.log(chalk[chalkColors[color]](camarade));
 });
-
-
