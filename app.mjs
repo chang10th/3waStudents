dd2aea2867337e25fe28a871c91141ffbc5cda6d
chang10th <chang10thomas@gmail.com>
2023-10-25 12:04:44 +0200

    chalk app.mjs correctif

--- app.mjs
+++ app.mjs
@@ -1,10 +1,12 @@
-import chalk from './node_modules/chalk';
+import chalk from "chalk";
 
 const camarades = ['Elie', 'Hava', 'Jérôme', 'Jayson'];
+
+const chalkColors = ['red','cyanBright','greenBright','magenta','yellow','cyan',];
 
 camarades.forEach((camarade, index) => {
   const color = index % 6 + 1;
   console.log(chalk.keyword(chalkColors[color])(camarade));
 });
 
-const chalkColors = ['red','cyanBright','greenBright','magenta','yellow','cyan',];
+
